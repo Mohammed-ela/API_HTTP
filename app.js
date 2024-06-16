ad76a70f2004c158690790b3ce42720e669cba3d
Mohammed <elamrani.mohammed95@gmail.com>
2024-06-16 15:28:51 +0200

    correction & lancement du serveur

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const express = require("express")
-const port = process.env.PORT || 5000
+const express = require("express");
+const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.lister(port,{} => {
-    console.log("serveur en ligne sur le port :",port)
-})
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(port, () => {
+    console.log(`Serveur en ligne sur le port : ${port}`);
+});
